7a351235d5edf0ace28c4d514a41a2c9c357ac64
Serg <example@gmail.com>
2022-03-22 14:37:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+/*const obj = {
+    name: "Alex",
+    age:22,
+    high: 176
+};
+
+console.log(obj.age);
+
+"use sctrict";
+
+alert('Hello! Welcome JS!!');
+*/
+
+//const result = confirm("Are you here");
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+//console.log(answer + 5);
+
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+//document.write(answers);
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+"use strict";
+
+
+/*const category = 'toys';
+
+console.log(`https:://it48.com/${category}/5`);
+
+const user = "Serg";
+ alert(`Привет, ${user}`); */
+
+
+console.log(4 + +"5");
+ 
+let incr = 10,
+    decr = 10;
+
+//    ++incr;
+//    --decr;
+
+   /* console.log(++incr);
+    console.log(--decr);
+
+    console.log(7%7);
+
+    console.log(2*4 === '8'); */
+
+&& - оператор И
+|| - оператор ИЛИ
+
+const isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
+
